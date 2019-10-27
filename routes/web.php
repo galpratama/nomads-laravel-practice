9545a5749911d2a8f5a86cd1c5909b46aafd69bd
Galih Pratama <hanamura.iost@gmail.com>
2019-10-28 00:38:58 +0700

    Tambah middleware admin

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('/checkout/success', 'CheckoutController@success')
 
 Route::prefix('admin')
     ->namespace('Admin')
+    ->middleware(['auth', 'admin'])
     ->group(function() {
         Route::get('/', 'DashboardController@index')
             ->name('dashboard');
