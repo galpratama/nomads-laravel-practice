22a6df445e38525967ec15aa79c6ee17ad99c5a9
Galih Pratama <hanamura.iost@gmail.com>
2019-10-29 15:53:18 +0700

    Kabeh

--- app/Http/Controllers/Admin/TravelPackageController.php
+++ app/Http/Controllers/Admin/TravelPackageController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\TravelPackageRequest;
+use App\TravelPackage;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class TravelPackageController extends Controller
 {
@@ -14,7 +17,11 @@ class TravelPackageController extends Controller
      */
     public function index()
     {
-        //
+        $items = TravelPackage::all();
+
+        return view('pages.admin.travel-package.index',[
+            'items' => $items
+        ]);
     }
 
     /**
@@ -24,7 +31,7 @@ class TravelPackageController extends Controller
      */
     public function create()
     {
-        //
+        return view('pages.admin.travel-package.create');
     }
 
     /**
@@ -33,9 +40,13 @@ class TravelPackageController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(TravelPackageRequest $request)
     {
-        //
+        $data = $request->all();
+        $data['slug'] = Str::slug($request->title);
+
+        TravelPackage::create($data);
+        return redirect()->route('travel-package.index');
     }
 
     /**
@@ -57,7 +68,11 @@ class TravelPackageController extends Controller
      */
     public function edit($id)
     {
-        //
+        $item = TravelPackage::findOrFail($id);
+
+        return view('pages.admin.travel-package.edit',[
+            'item' => $item
+        ]);
     }
 
     /**
@@ -67,9 +82,16 @@ class TravelPackageController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(TravelPackageRequest $request, $id)
     {
-        //
+        $data = $request->all();
+        $data['slug'] = Str::slug($request->title);
+
+        $item = TravelPackage::findOrFail($id);
+
+        $item->update($data);
+
+        return redirect()->route('travel-package.index');
     }
 
     /**
@@ -80,6 +102,10 @@ class TravelPackageController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $item = TravelPackage::findorFail($id);
+        $item->delete();
+
+        return redirect()->route('travel-package.index');
+
     }
 }
